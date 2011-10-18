a6ac59df77fa4a28a85e33ad59e61ad4eb79254e
kubota <kubota@pankaku.com>
2011-10-18 18:48:02 +0900

    tmp commit

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Block : MonoBehaviour {
+    StageState state;
+
+    IEnumerator<float> shake;
+
+    public void Shake() {
+        GameObject game = GameObject.Find("Game");
+        this.state = game.GetComponent<StageState>();
+    }
+
     // Use this for initialization
-    void Start () {
+    void Start() {
     }
     
     // Update is called once per frame
-    void Update () {
+    void Update() {
     }
+
+    // IEnumerator<float> ShakeStart() {
+    // }
 }
--- Assets/Scripts/StageState.cs
+++ Assets/Scripts/StageState.cs
@@ -24,4 +24,6 @@ public class StageState : MonoBehaviour {
 
     public float walkSpeed = 3.0f;
     public float digTimeRate = 0.5f;
+
+    public float blockShakeTime = 0.2f;
 }
